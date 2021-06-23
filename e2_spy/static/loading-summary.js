d472f488f32ff5671e9aba15848cd87ea8ad0de3
William Jackson <william@subtlecoolness.com>
2021-06-22 22:35:07 -0500

    Bump bootstrap to 5.0.2

--- e2_spy/static/loading-summary.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('select-department').addEventListener('change', function (ev) {
-    const selected_option = this.options[this.selectedIndex];
-    const url_params = new URLSearchParams(window.location.search);
-    url_params.set('department', selected_option.value);
-    window.location.search = url_params;
-});
